23b564ad584248747aac6d0e4a42735df451a6f5
Ville <vipeeri@gmail.com>
2017-02-02 16:53:21 +0200

    edited task.php

--- app/models/task.php
+++ app/models/task.php
@@ -8,6 +8,55 @@ class Task extends BaseModel {
     // Konstruktori
     public function __construct($attributes) {
         parent::__construct($attributes);
+        $siivous = new Task(array('id' => 1, 'name' => 'The Elder Scrolls X: Siivoa', 'description' => 'siivoa wc'));
+    }
+
+    public static function all() {
+        // Alustetaan kysely tietokantayhteydellämme
+        $query = DB::connection()->prepare('SELECT * FROM Task');
+        // Suoritetaan kysely
+        $query->execute();
+        // Haetaan kyselyn tuottamat rivit
+        $rows = $query->fetchAll();
+        $tasks = array();
+
+        // Käydään kyselyn tuottamat rivit läpi
+        foreach ($rows as $row) {
+            // Tämä on PHP:n hassu syntaksi alkion lisäämiseksi taulukkoon :)
+            $tasks[] = new Task(array(
+                'id' => $row['id'],
+                'task_id' => $row['task_id'],
+                'name' => $row['name'],
+                'description' => $row['description'],
+                'priority' => $row['priority'],
+                'added' => $row['added'],
+                'deadline' => $row['deadline'],
+                'category' => $row['category']
+            ));
+        }
+
+        return $tasks;
+    }
+
+    public static function find($id) {
+        $query = DB::connection()->prepare('SELECT * FROM Task WHERE id = :id LIMIT 1');
+        $query->execute(array('id' => $id));
+        $row = $query->fetch();
+
+        if ($row) {
+            $task = new Task(array(
+                'id' => $row['id'],
+                'task_id' => $row['task_id'],
+                'name' => $row['name'],
+                'description' => $row['description'],
+                'priority' => $row['priority'],
+                'added' => $row['added'],
+                'deadline' => $row['deadline'],
+                'category' => $row['category']
+            ));
+
+            return $task;
+        }
     }
 
 }
